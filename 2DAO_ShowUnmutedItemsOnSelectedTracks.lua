12ac8873439788c7cb92b57f9fa8b5ab2e6e808a
daodaodai <88189849+daodaodai@users.noreply.github.com>
2021-09-28 14:34:45 +0100

    Add files via upload
    改进

--- 2DAO_ShowUnmutedItemsOnSelectedTracks.lua
+++ 2DAO_ShowUnmutedItemsOnSelectedTracks.lua
@@ -4,6 +4,8 @@
 					本意是方便我看看音乐轨道上都用了什么配乐。
 	Modified by Daodao.
 	Date: 2021.09.06
+	Modified. date. 2021.09.28 增加 sorting 功能，增加判断，如果两个比邻的片段是一个文件，那它算一个音乐文件拼接成新的，就显示一次。
+				如果同一个音乐文件在不同的位置出现，那算被多次使用，最后也参与显示
 --]]
 
 
@@ -69,9 +71,12 @@ reaper.Main_OnCommand( 40421, 0 )  -- command: Item: Select all items in track
 
 tblAllUnmutedItems = {}
 tblAllUnmuted_Times = {}
+tblAllUnmuted_TimesRaw = {}
+tblAllUnmuted_EndTimes = {}
+tblAll_TimeAndNames = {}
 entryCount = 0
 outputStr1 = ""
-outputStr2 = ""
+outputStr2 = "没静音的文件 in the selected tracks:\n---------------------------------------\n\n"
 previousItemName = ""
 j = 0
 
@@ -82,29 +87,35 @@ numOfSelectedItems = reaper.CountSelectedMediaItems( thisProject ) -- 0: this pr
 for i = 0, numOfSelectedItems - 1 do
 	local item = reaper.GetSelectedMediaItem( thisProject, i)
 	local isMute = reaper.GetMediaItemInfo_Value( item, "B_MUTE" )
-	local startPos = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
-	local startPosReadable = DecimalsToMinutes(startPos)
+	local startPos = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )	
+	--local startPosReadable = DecimalsToMinutes(startPos)
+	local timeLen = reaper.GetMediaItemInfo_Value( item, "D_LENGTH" )
+	local endPos = startPos + timeLen
 	
 	if (isMute == bUnmute) then
 		local take = reaper.GetActiveTake(item)
 		if take and not reaper.TakeIsMIDI( take ) then
 			local takeName = reaper.GetTakeName( take )
 			
-			if (takeName ~= previousItemName) then
+			--if (takeName ~= previousItemName) then
 				--outputStr1 = startPos.."  "..outputStr1..takeName.."\n"
-				tblAllUnmuted_Times[j] = startPosReadable
+				-- 记录：新片段的时间点、名字、终止时间
+				tblAllUnmuted_Times[j] = startPos
+				--tblAllUnmuted_TimesRaw[j] = startPos
 				previousItemName = takeName
 				tblAllUnmutedItems[j] = takeName
 				--showMsg(tblAllUnmutedItems[j], "新增take name")
+				tblAll_TimeAndNames[startPos] = takeName
+				tblAllUnmuted_EndTimes[startPos] = endPos
 				j = j + 1
-			end	
+			--end	
 		end
 	end
 end
 
-outputStr_3 = "\n\n--------------详情---------------\n\n"
+outputStr_3 = "\n\n--------------详情，按轨道顺序列举所有片段---------------\n\n"
 for k = 0, j - 1 do
-	outputStr_3 = outputStr_3.."\n"..tblAllUnmuted_Times[k].."   "..tblAllUnmutedItems[k]
+	outputStr_3 = outputStr_3.."\n"..DecimalsToMinutes(tblAllUnmuted_Times[k]).."   "..tblAllUnmutedItems[k]
 end
 
 
@@ -119,18 +130,54 @@ for i = 0, entryCount do
 	local strTemp = tblAllUnmutedItems[i]
 	
 	if strTemp == nil and i == 0 then
-		showMsg2("轨道上没有 没静音 的文件. No unmuted files found on tracks selected.")
 		bFound = false
 	else
 		if (strTemp ~= previousItemName) then
+			-- outputStr2 存的是 unique file names，重复的不存入
 			outputStr2 = outputStr2.." ** "..strTemp.."\n"
 			previousItemName = strTemp
 		end
 	end
 end
 
-if bFound and outputStr2 then
-	outputStr2 = "所有没静音的文件 in the selected tracks:\n".."---------------------------------------\n\n"..outputStr2
+--if bFound and outputStr2 then
+	--outputStr2 = "所有没静音的文件 in the selected tracks:\n".."---------------------------------------\n\n"..outputStr2
 	--reaper.ShowConsoleMsg(outputStr2)
-	reaper.ShowConsoleMsg(outputStr2..outputStr_3)
+--	reaper.ShowConsoleMsg(outputStr2..outputStr_3)
+--end
+
+-- 按时间顺序给音乐文件列表
+table.sort(tblAllUnmuted_Times)
+entryCount = #tblAllUnmuted_Times
+outputStr_4 = "\n---------------------------------------\n轨道上没有没静音的文件，time sorted.\n---------------------------------------\n\n"
+strName = ""
+strTime = ""
+strPreviousItem_endTime = "0"
+strPreviousItem_name = ""
+
+
+if bFound then
+	for i = 0, entryCount do
+		strTime = tblAllUnmuted_Times[i]
+		strName = tblAll_TimeAndNames[strTime]
+		strEndTime = tblAllUnmuted_EndTimes[strTime]
+		if (strName == strPreviousItem_name) then
+			-- 可能是同一条音乐剪切拼接
+			if (strTime > strPreviousItem_endTime) then
+				-- 如果这个item的起始时间，> 前面一个 item的 end time，这可能是音乐被多次使用而不是被拼接成一个。这个情况，显示它
+				outputStr_4 = outputStr_4..DecimalsToMinutes(strTime).."  "..strName.."\n"	
+			end
+		else
+			outputStr_4 = outputStr_4..DecimalsToMinutes(strTime).."  "..strName.."\n"	
+		end
+		
+		-- 已经决定了是否要显示这个 item了。现在把它的信息存起来，为下一个比较做准备
+		strPreviousItem_endTime = strEndTime
+		strPreviousItem_name = strName
+	end	
+	
+	reaper.ShowConsoleMsg(outputStr2..outputStr_4..outputStr_3)
+else
+	showMsg2("轨道上没有 没静音 的文件. No unmuted files found on tracks selected.")
 end
+
