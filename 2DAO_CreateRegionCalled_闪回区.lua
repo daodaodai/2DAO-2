750cf8f8560b3005fbf998088d70671d10bcdb12
daodaodai <88189849+daodaodai@users.noreply.github.com>
2022-01-09 04:15:44 +0000

    Add files via upload

---
+++ 2DAO_CreateRegionCalled_闪回区.lua
@@ -0,0 +1,43 @@
+--[[
+ * ReaScript Name: 2DAO_CreateRegionCalled_混响区.lua
+ * Description: create my region called 混响区.
+ * Author: 2Dao
+ * Date: 2021.08.04
+--]]
+
+
+
+newRegionName = "闪回区"
+
+function showSimpleMessage(strMsg, strTitle)
+
+	reaper.ShowMessageBox( strMsg, strTitle, 0)
+
+end
+
+function main()
+
+	reaper.Main_OnCommand( 40174, 0 )  -- Markers: Insert region from time selection
+	
+	cursorPosition = reaper.GetCursorPosition() -- this would be the start of the region
+	local thisProject = 0  -- 0 is the current project
+	markerIdx, regionIdx = reaper.GetLastMarkerAndCurRegion(thisProject, cursorPosition)
+	
+	local ret, bIsRegion, startPos, endPos, name, markrgnindexnumber = reaper.EnumProjectMarkers(regionIdx)
+	reaper.SetProjectMarkerByIndex(0, regionIdx, bIsRegion, startPos, endPos, markrgnindexnumber, newRegionName, 0)
+
+end
+
+
+
+reaper.PreventUIRefresh(1)
+
+reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+main()
+
+reaper.Undo_EndBlock("Create named region", - 1) -- End of the undo block. Leave it at the bottom of your main function.
+
+reaper.UpdateArrange()
+
+reaper.PreventUIRefresh(-1)
