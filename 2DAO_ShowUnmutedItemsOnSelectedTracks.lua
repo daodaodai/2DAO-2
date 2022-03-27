d7d590a54c086ef7bcd060759598a9c50c431ea4
daodaodai <88189849+daodaodai@users.noreply.github.com>
2022-03-27 07:52:14 +0100

    add more text to the display

--- 2DAO_ShowUnmutedItemsOnSelectedTracks.lua
+++ 2DAO_ShowUnmutedItemsOnSelectedTracks.lua
@@ -76,7 +76,8 @@ tblAllUnmuted_EndTimes = {}
 tblAll_TimeAndNames = {}
 entryCount = 0
 outputStr1 = ""
-outputStr2 = "---------------------------------------\n没静音的文件 in the selected tracks:\n---------------------------------------\n\n"
+strProjName = reaper.GetProjectName(thisProject)
+outputStr2 = "        "..strProjName.."\n---------------------------------------\n没静音的文件 in the selected tracks:\n---------------------------------------\n\n"
 previousItemName = ""
 j = 0
 
